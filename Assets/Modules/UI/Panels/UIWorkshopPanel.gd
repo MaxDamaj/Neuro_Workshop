e7c94d5f4e2043d1c4c00d8fe54fb72a0b6538b3
Ismail <iadjiev@gmail.com>
2024-12-29 00:11:47 +0300

    Added ingame settings screen UI

--- Assets/Modules/UI/Panels/UIWorkshopPanel.gd
+++ Assets/Modules/UI/Panels/UIWorkshopPanel.gd
@@ -3,11 +3,19 @@ class_name UIWorkshopPanel
 
 @onready var _uiPanelsProvider : UIPanelsProvider = get_node(UIPanelsProvider.path)
 
-@export var BackButton : Button
+@export var SettingsButton : Button
+@export var CloseSettingsButton : Button
+@export var MainMenuButton : Button
+@export var SettingsContainer : Control
 
 func _ready() -> void:
-	BackButton.button_down.connect(_try_close_level)
+	SettingsContainer.visible = false
+	SettingsButton.button_down.connect(_switch_settings_panel)
+	CloseSettingsButton.button_down.connect(_switch_settings_panel)
+	MainMenuButton.button_down.connect(_try_close_level)
 
+func _switch_settings_panel():
+	SettingsContainer.visible = !SettingsContainer.visible
 
 func _try_close_level():
 	_uiPanelsProvider.open_panel("exit_level_ui")
