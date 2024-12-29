bf9e58081158653c24b93eebf7e79a30d8d3e7fc
Юрий Молчанов <maxdamaj@yandex.ru>
2024-12-29 21:40:43 +0300

    Новый ведал, графика баббла у нпс

---
+++ Assets/Modules/UI/Views/TileBubble.gd
@@ -0,0 +1,13 @@
+extends Node
+class_name TileBubble
+
+@export var OrderIcon : TextureRect
+@export var ProgressSlider : TextureProgressBar
+@export var ColorGrad : GradientTexture1D
+
+
+func _ready() -> void:
+	ProgressSlider.value_changed.connect(_change_color)
+
+func _change_color(value : float):
+	ProgressSlider.modulate = ColorGrad.gradient.sample(value / ProgressSlider.max_value)
