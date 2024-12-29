32a8602ae0981179746dbe40cf71879b4e49cad1
Ismail <iadjiev@gmail.com>
2024-12-29 22:31:51 +0300

    Changed in game screens to match the mockup

--- Assets/Modules/UI/Panels/UIExitLevelPanel.gd
+++ Assets/Modules/UI/Panels/UIExitLevelPanel.gd
@@ -4,6 +4,7 @@ class_name UIExitLevelPanel
 @onready var _levelLoadStartegy : LevelLoadStrategy = get_node(LevelLoadStrategy.path)
 @onready var _uiPanelsProvider : UIPanelsProvider = get_node(UIPanelsProvider.path)
 
+@export var CloseButton : Button
 @export var ExitButton : Button
 @export var ReturnButton : Button
 
@@ -11,6 +12,7 @@ class_name UIExitLevelPanel
 func _ready() -> void:
 	ExitButton.button_down.connect(_exit_level)
 	ReturnButton.button_down.connect(func(): _uiPanelsProvider.close_panel("exit_level_ui"))
+	CloseButton.button_down.connect(func(): _uiPanelsProvider.close_panel("exit_level_ui"))
 
 
 func _exit_level():
